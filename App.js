6ee4616d559f7a493a39cdfa38222170d6f8f689
Manali Patki <Manali.patki@atcs.com>
2023-07-13 13:21:52 +0530

    adding files

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+const parent = React.createElement(
+  "div",
+  { id: "paremt" },
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("div", { id: "child1" }, "Hello World from parent"),
+    React.createElement("div", { id: "child1" }, "Hello wrld"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("div", { id: "child2" }, "Hello World from parent"),
+    React.createElement("div", { id: "child2" }, "Hello World from parent 2"),
+  ])
+);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React"
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
